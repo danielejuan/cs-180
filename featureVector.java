9f758437b1cf6d5cba3a55588869313c14c55a92
jedpatrickdatu <jedpatrickdatu@yahoo.com>
2015-05-27 16:26:33 +0800

    Created the FeatureVector.java class.

--- featureVector.java
+++ featureVector.java
@@ -2,6 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class featureVector {
-	String name;
+	String age; 
+	String sex; 
+	String cp; 
+	String trestbps; 
+	String chol; 
+	String fbs; 
+	String restecg; 
+	String thalach; 
+	String exang; 
+	String oldpeak; 
+	String slope; 
+	String ca; 
+	String thal; 
+	String num;
+
+	public List<String> getList(){
+		ArrayList<String> values = new ArrayList<String>;
+		values.add("age=" + age);
+		values.add("sex=" + sex);
+		values.add("cp=" + cp);
+		values.add("trestbps=" + trestbps);
+		values.add("chol=" + chol);
+		values.add("fbs=" + fbs);
+		values.add("restecg=" + restecg);
+		values.add("thalach=" + thalach);
+		values.add("exang=" + exang);
+		values.add("oldpeak=" + oldpeak);
+		values.add("slope=" + slope);
+		values.add("ca=" + ca);
+		values.add("thal=" + thal);
+		values.add("num=" + num);
+		return Arrays.asList(values.toArray(new String[values.size()]));
+	}
 	
 }
